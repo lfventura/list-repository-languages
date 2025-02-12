30d9702dddd5d35b278b7d7f78e8f351504818d4
Lucas Ventura <lfventura@users.noreply.github.com>
2025-02-12 02:39:38 -0300

    Adds Build Mode Logic (#2)
    * testing return array
    
    * testing return array
    
    * Add Build Mode logic
    
    * update workflow to test new functionality
    
    * update workflow name
    
    * update workflow name
    
    * add missing inputs
    
    * fixes
    
    * rollback
    
    * several minor fixes
    
    * add missing output on workflow
    
    * update action.yml
    
    * run less cron

--- README.md
+++ README.md
@@ -60,15 +60,36 @@ Various inputs are defined in [`action.yml`](action.yml):
 | github&#x2011;token | Token to use to authorize. | ${{&nbsp;github.token&nbsp;}} |
 | owner | The repository owner | ${{ github.repository_owner }} |
 | repo | The repository name | ${{ github.event.repository.name }} |
+| buildmode_cpp | Custom build mode for C++ | none |
+| buildmode_csharp | Custom build mode for C# | none |
+| buildmode_go | Custom build mode for Go | none |
+| buildmode_javakotlin | Custom build mode for Java/Kotlin | none |
+| buildmode_js | Custom build mode for JavaScript/TypeScript | none |
+| buildmode_python | Custom build mode for Python | none |
+| buildmode_ruby | Custom build mode for Ruby | none |
+| buildmode_swift | Custom build mode for Swift | none |
+| buildmode_manual_cpp | Build command to be run when manual build mode set for C++ | none |
+| buildmode_manual_csharp | Build command to be run when manual build mode set for C# | none |
+| buildmode_manual_go | Build command to be run when manual build mode set for Go | none |
+| buildmode_manual_javakotlin | Build command to be run when manual build mode set for Java/Kotlin | none |
+| buildmode_manual_js | Build command to be run when manual build mode set for JavaScript/TypeScript | none |
+| buildmode_manual_python | Build command to be run when manual build mode set for Python | none |
+| buildmode_manual_ruby | Build command to be run when manual build mode set for Ruby | none |
+| buildmode_manual_swift | Build command to be run when manual build mode set for Swift | none |
 
 
 ## ⬅️ Outputs
 | Name | Description |
 | --- | - |
-| languages | The languages of the repository as a JSON array |
-| languages_codeql | The languages of the repository as a JSON array for CodeQL Matrix |
+| languages_repo | The languages of the repository as a JSON array |
+| languages_codeql | The languages of the repository as a JSON array for CodeQL Matrix without Build Mode set |
+| languages_codeql_w_buildmode | The languages of the repository as a JSON array for CodeQL Matrix with Build Mode set |
 | codeql_supported | Bool that indicates if there are supported languages by CodeQL |
 
+
+## Examples
+
+Check .github/workflow folder for some examples on how to use.
 
 ## Further help
 To get more help on the Actions see [documentation](https://docs.github.com/en/actions).
--- src/run.ts
+++ src/run.ts
@@ -12,6 +12,7 @@ export async function run(): Promise<void> {
     const owner: string = core.getInput('owner')
     const repo: string = core.getInput('repo')
 
+    // Languages that are supported by CodeQL, mapping between the Github API output and the accepted CodeQL Input
     const codeqlLanguageMapping: { [key: string]: string } = {
       // https://docs.github.com/en/code-security/code-scanning/creating-an-advanced-setup-for-code-scanning/customizing-your-advanced-setup-for-code-scanning#changing-the-languages-that-are-analyzed
       // https://aka.ms/codeql-docs/language-support
@@ -28,13 +29,76 @@ export async function run(): Promise<void> {
       "ruby": "ruby",
       "swift": "swift",
     }
+
+    // Mapping between the CodeQL languages and the default build mode for CodeQL
+    let codeqlBuildmodeMapping: { [key: string]: string } = {
+      "c-cpp": "none",
+      "csharp": "none",
+      "go": "none",
+      "java-kotlin": "none",
+      "javascript-typescript": "none",
+      "python": "none",
+      "ruby": "none",
+      "swift": "none",
+    }
+
+    // If there is an input for the module passing a custom build mode, store on this transitive const
+    const customBuildmode : { [key: string]: string } = {
+      "c-cpp": core.getInput('buildmode_cpp'),
+      "csharp": core.getInput('buildmode_csharp'),
+      "go": core.getInput('buildmode_go'),
+      "java-kotlin": core.getInput('buildmode_javakotlin'),
+      "javascript-typescript": core.getInput('buildmode_js'),
+      "python": core.getInput('buildmode_python'),
+      "ruby": core.getInput('buildmode_ruby'),
+      "swift": core.getInput('buildmode_swift'),
+    }
+
+    // If there is a custom build mode, update the default build mode mapping
+    for (const language in customBuildmode) {
+      if (customBuildmode[language]) {
+        codeqlBuildmodeMapping[language] = customBuildmode[language]
+      }
+    }
+
+    // If the user set a manual build mode, we are going to need which command should be ran for each language
+    // Definining the default manual build mode command
+    const defaultBuildModeManualCommand : string = "echo 'When you are running manual build mode, you need to provide the build steps over input buildmode_manual_<lang> to the list-repository-languages module' && exit 1"
+
+    // If there is an input for the module passing a custom manual build mode command, store on this transitive const
+    const manualBuildmodeCommand : { [key: string]: string } = {
+      "c-cpp": core.getInput('buildmode_manual_cpp') || defaultBuildModeManualCommand,
+      "csharp": core.getInput('buildmode_manual_csharp') || defaultBuildModeManualCommand,
+      "go": core.getInput('buildmode_manual_go') || defaultBuildModeManualCommand,
+      "java-kotlin": core.getInput('buildmode_manual_javakotlin') || defaultBuildModeManualCommand,
+      "javascript-typescript": core.getInput('buildmode_manual_js') || defaultBuildModeManualCommand,
+      "python": core.getInput('buildmode_manual_python') || defaultBuildModeManualCommand,
+      "ruby": core.getInput('buildmode_manual_ruby') || defaultBuildModeManualCommand,
+      "swift": core.getInput('buildmode_manual_swift') || defaultBuildModeManualCommand,
+    }
+
+    // If there is a custom manual build mode command, update the default manual build mode command mapping
+    let customManualBuildmodeCommand : { [key: string]: string } = {}
+    for (const language in customBuildmode) {
+      if (customBuildmode[language] == "manual") {
+        customManualBuildmodeCommand[language] = manualBuildmodeCommand[language]
+      }
+    }
+
     const octokit: ReturnType<typeof github.getOctokit> = github.getOctokit(token);
     const langResponse = await octokit.request(`GET /repos/${owner}/${repo}/languages`);
     core.debug(JSON.stringify({langResponse}))
     let languages: string[] = Object.keys(langResponse.data);
     let languages_codeql_format = Array.from(new Set(languages.map(l => codeqlLanguageMapping[l.toLowerCase()]).filter(l => l)));
+    let languages_codeql_output = languages_codeql_format.map(language => ({
+      language: language,
+      "build-mode": codeqlBuildmodeMapping[language],
+      "manual-build-command": customManualBuildmodeCommand[language] || ""
+    }));
+
     core.setOutput('languages_repo', JSON.stringify(languages.map(l => l.toLowerCase())));
     core.setOutput('languages_codeql', JSON.stringify(languages_codeql_format));
+    core.setOutput('languages_codeql_w_buildmode', JSON.stringify(languages_codeql_output));
     core.setOutput('codeql_supported', JSON.stringify(languages_codeql_format.length > 0));
 
   } catch (error) {

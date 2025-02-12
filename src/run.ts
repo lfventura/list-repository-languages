e823b32309dae3966ec81d08980aba98fb072e1b
Lucas Ventura <lfventura@users.noreply.github.com>
2025-02-11 23:09:29 -0300

    Update structure for the new action structure, enable npx local action to test the action locally

--- src/run.ts
+++ src/run.ts
@@ -1,52 +1,41 @@
-import * as core from '@actions/core';
+import * as core from '@actions/core'
 import * as github from '@actions/github';
 
-interface Input {
-  token: string;
-  owner: string;
-  repo: string;
-  codeql: boolean;
-}
-
-export function getInputs(): Input {
-  const result = {} as Input;
-  result.token = core.getInput('github-token');
-  result.owner = core.getInput('owner');
-  result.repo = core.getInput('repo');
-  result.codeql = core.getBooleanInput('codeql');
-  return result;
-}
-
-const codeqlLanguageMapping = {
-	"csharp": "csharp",
-	"c#": "csharp",
-	"cpp": "cpp",
-	"c": "cpp",
-	"c++": "cpp",
-	"go": "go",
-	"java": "java",
-	"javascript": "javascript",
-	"typescript": "javascript",
-	"python": "python",
-	"ruby": "ruby"
-}
-
-const run = async (): Promise<void> => {
+/**
+ * The main function for the action.
+ *
+ * @returns Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
   try {
-    const input = getInputs();
-    const octokit: ReturnType<typeof github.getOctokit> = github.getOctokit(input.token);
-    const langResponse = await octokit.request(`GET /repos/${input.owner}/${input.repo}/languages`);
-    core.debug(JSON.stringify({langResponse}))
-    let languages = Object.keys(langResponse.data);
-    if (input.codeql) {
-      languages = languages.filter(l => codeqlLanguageMapping[l.toLowerCase()]);
-    }
-    core.setOutput('languages', JSON.stringify(languages.map(l => l.toLowerCase())));
-  } catch (error) {
-    core.startGroup(error instanceof Error ? error.message : JSON.stringify(error));
-    core.info(JSON.stringify(error, null, 2));
-    core.endGroup();
-  }
-};
+    const token: string = core.getInput('github-token')
+    const owner: string = core.getInput('owner')
+    const repo: string = core.getInput('repo')
 
-export default run;
+    const codeqlLanguageMapping: { [key: string]: string } = {
+      "c": "c-cpp",
+      "c++": "c-cpp",
+      "cpp": "c-cpp",
+      "csharp": "csharp",
+      "go": "go",
+      "java": "java-kotlin",
+      "kotlin": "java-kotlin",
+      "javascript": "javascript-typescript",
+      "typescript": "javascript-typescript",
+      "python": "python",
+      "ruby": "ruby",
+      "swift": "swift",
+    }
+    const octokit: ReturnType<typeof github.getOctokit> = github.getOctokit(token);
+    const langResponse = await octokit.request(`GET /repos/${owner}/${repo}/languages`);
+    core.debug(JSON.stringify({langResponse}))
+    let languages: string[] = Object.keys(langResponse.data);
+    let languages_codeql_format = Array.from(new Set(languages.map(l => codeqlLanguageMapping[l.toLowerCase()]).filter(l => l)));
+    core.setOutput('languages_repo', JSON.stringify(languages.map(l => l.toLowerCase())));
+    core.setOutput('languages_codeql', JSON.stringify(languages_codeql_format));
+
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
